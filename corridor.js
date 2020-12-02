45b78d3cf4bd86d14230a894f2f87bd585054e2f
mousqtr <49991720+mousqtr@users.noreply.github.com>
2020-12-02 16:30:27 +0100

    Separate file for corridor

---
+++ corridor.js
@@ -0,0 +1,246 @@
+import * as THREE from 'https://threejsfundamentals.org/threejs/resources/threejs/r122/build/three.module.js';
+import {FBXLoader} from 'https://cdn.jsdelivr.net/npm/three@0.118.1/examples/jsm/loaders/FBXLoader.js';
+
+export function loadCorridor(scene){
+
+    let objects = {};
+    let materials = {}
+    let mixers = {}
+    let actions = {};
+    let lights = {};
+
+
+    /*************************************************************/
+    /*                          LIGHTS                           */
+    /*************************************************************/
+
+    // Hemisphere Light
+    const hemiLight = new THREE.HemisphereLight( 0xffffff, 0x444444 );
+    hemiLight.position.set(0, 0, 0);
+    scene.add( hemiLight );
+    lights["hemiLight"] = hemiLight;
+
+    // Directional Light
+    const dirLight = new THREE.DirectionalLight( 0xffffff );
+    dirLight.position.set(0, 0, 300);
+    dirLight.castShadow = true;
+    dirLight.shadow.camera.top = 180;
+    dirLight.shadow.camera.bottom = - 100;
+    dirLight.shadow.camera.left = - 120;
+    dirLight.shadow.camera.right = 120;
+    scene.add( dirLight );
+    lights["dirLight"] = dirLight;
+
+    /*************************************************************/
+    /*                       CORRIDOR                            */
+    /*************************************************************/
+
+    let corridorMaterialArray = [];
+    let texture_ft = new THREE.TextureLoader().load( 'img/wall2.jpg');
+    let texture_bk = new THREE.TextureLoader().load( 'img/wall2.jpg');
+    let texture_up = new THREE.TextureLoader().load( 'img/wall3.jpg');
+    let texture_dn = new THREE.TextureLoader().load( 'img/parquet.jpg');
+    let texture_rt = new THREE.TextureLoader().load( 'img/wall2.jpg');
+    let texture_lf = new THREE.TextureLoader().load( 'img/wall2.jpg');
+
+    texture_dn.wrapS = texture_dn.wrapT = THREE.RepeatWrapping;
+    texture_up.wrapS = texture_up.wrapT = THREE.RepeatWrapping;
+    texture_ft.wrapS = texture_ft.wrapT = THREE.RepeatWrapping;
+    texture_bk.wrapS = texture_bk.wrapT = THREE.RepeatWrapping;
+
+    texture_dn.repeat.y = 5;
+    texture_up.repeat.y = 5;
+    texture_ft.repeat.x = 5;
+    texture_bk.repeat.x = 5;
+
+    corridorMaterialArray.push(new THREE.MeshBasicMaterial( { map: texture_ft }));
+    corridorMaterialArray.push(new THREE.MeshBasicMaterial( { map: texture_bk }));
+    corridorMaterialArray.push(new THREE.MeshBasicMaterial( { map: texture_up }));
+    corridorMaterialArray.push(new THREE.MeshBasicMaterial( { map: texture_dn }));
+    corridorMaterialArray.push(new THREE.MeshBasicMaterial( { map: texture_rt }));
+    corridorMaterialArray.push(new THREE.MeshBasicMaterial( { map: texture_lf }));
+
+    for (let i = 0; i < 6; i++) {
+        corridorMaterialArray[i].side = THREE.BackSide;
+    }
+        
+    let corridorGeo = new THREE.BoxGeometry( 1000, 1000, 10000);
+    let corridor = new THREE.Mesh( corridorGeo, corridorMaterialArray );
+    corridor.position.set(0, 0, -4500)
+    scene.add( corridor );  
+    objects["corridor"] = corridor;
+
+
+    /*************************************************************/
+    /*                       PALADIN                             */
+    /*************************************************************/
+
+    const loaderPaladin = new FBXLoader();
+    loaderPaladin.load('models/paladin/paladin.fbx', (paladin) => {
+
+        // Load the model
+        paladin.scale.setScalar(3);
+        paladin.position.set(0, -500, -600)
+        paladin.rotation.set(0, Math.PI, 0)
+        paladin.traverse(child => {
+        child.castShadow = true;
+        child.receiveShadow = true;
+        });
+        objects["paladin"] = paladin;
+
+        // Load the walk animation
+        const animWalk = new FBXLoader();
+        animWalk.load('models/paladin/walk.fbx', (animWalk) => {
+            const mixer = new THREE.AnimationMixer(paladin);     
+            const action = mixer.clipAction( animWalk.animations[0] );
+            mixers["mixerWalk"] = mixer
+            actions["walk"] = action;
+        });
+        
+        // Load the stand animation
+        const animStand = new FBXLoader();
+        animStand.load('models/paladin/stand_brief.fbx', (animStand) => {
+            const mixer = new THREE.AnimationMixer(paladin);
+            const action = mixer.clipAction(animStand.animations[0]);
+            mixers["mixerStand"] = mixer
+            actions["stand"] = action;
+            action.play();
+        });
+
+        // Load the stand animation
+        const animRightTurn = new FBXLoader();
+        animRightTurn.load('models/paladin/right_turn.fbx', (animRightTurn) => {
+            const mixer = new THREE.AnimationMixer(paladin);
+            const action = mixer.clipAction(animRightTurn.animations[0]);
+            action.loop = THREE.LoopOnce;
+            action.clampWhenFinished = true;
+            mixers["mixerRightTurn"] = mixer
+            mixer.timeScale = 0.4;
+            mixer.addEventListener( 'finished', finishRightTurn );
+            actions["rightTurn"] = action;
+        });
+
+        scene.add(paladin);
+
+    });
+
+    let rightTurnFinished = false;
+
+    function finishRightTurn(){
+        rightTurnFinished = true;
+        actions["rightTurn"].reset()
+    }
+
+
+    /*************************************************************/
+    /*                          DOORS                            */
+    /*************************************************************/
+
+    let doors = [];
+    let doorPosX, doorPosZ;
+    for ( let i = 0; i < 4; i ++ ) {
+        const loaderDoor = new FBXLoader();
+        loaderDoor.load('models/door.fbx', (door) => {
+            
+            door.traverse(child => {
+                child.castShadow = true;
+                child.receiveShadow = true;
+                materials["door"] = child.material;
+                child.name = "door" + i.toString();
+            });
+            door.scale.setScalar(0.3);
+
+            if (i % 2 == 0){
+                doorPosX = -500;
+                door.rotation.set(0, Math.PI/2, 0);
+            }else{
+                doorPosX = 500;
+                door.rotation.set(0, -Math.PI/2, 0);
+            }
+        
+            if (i < 2){
+                doorPosZ = 0.67 * window.innerWidth - 2333;
+            }else{
+                doorPosZ = 0.67 * window.innerWidth - 4333;
+            }
+
+            door.position.set(doorPosX, -500, doorPosZ);
+            scene.add(door);
+            doors.push(door);
+        });
+    }
+    objects["doors"] = doors;
+
+
+    /*************************************************************/
+    /*                         TEXTS                             */
+    /*************************************************************/
+
+    let doorTexts = [];
+    let doorTextPosX, doorTextPosZ;
+    let texts = ['Profil', 'Formation', 'Experiences', 'Projets']
+    for ( let i = 0; i < 4; i ++ ) {
+        const loaderText = new THREE.FontLoader();
+        loaderText.load( 'fonts/Bebas_Neue_Regular.json', function ( font ) {
+
+            const textGeometry = new THREE.TextGeometry( texts[i], {
+                font: font, size: 60, height: 2,
+            });
+            
+            var textMaterial = new THREE.MeshPhongMaterial( { 
+                color: 0x00ff00, 
+            });
+            
+            var mesh = new THREE.Mesh( textGeometry, textMaterial );
+
+            if (i % 2 == 0){
+                doorTextPosX = -480;
+            }else{
+                doorTextPosX = 210;
+            }
+
+            if (i < 2){
+                doorTextPosZ = 0.67 * window.innerWidth - 1933;
+            }else{
+                doorTextPosZ = 0.67 * window.innerWidth - 3933;
+            }
+
+            mesh.position.set(doorTextPosX, 150, doorTextPosZ)
+            scene.add( mesh );
+
+            
+            doorTexts.push(mesh);
+        } );
+    }
+    objects["doorTexts"] = doorTexts;
+
+
+    /*************************************************************/
+    /*                         ARROW                             */
+    /*************************************************************/
+
+    // Load the top arrow 
+    const arrowLoader = new FBXLoader();
+    arrowLoader.load('models/arrow.fbx', (arrow) => {
+        arrow.traverse(child => {
+        child.castShadow = true;
+        child.receiveShadow = true;
+        child.material = new THREE.MeshPhongMaterial( { 
+            color: 0xff0000
+        } );
+        });       
+        arrow.scale.setScalar(200);
+        arrow.position.set(0, 200, -600);
+        arrow.rotation.set(-Math.PI/2, Math.PI, Math.PI/2);
+        arrow.name = "arrow";
+        objects["arrow"] = arrow;
+        scene.add(arrow);
+    });
+
+
+    return [objects, materials, mixers, actions, lights];
+
+}
+
+
+
--- main.js
+++ main.js
@@ -8,13 +8,12 @@ import {TGALoader} from 'https://cdn.jsdelivr.net/npm/three@0.118.1/examples/jsm
 import { walkTo, stopWalk } from "./characterMovements.js";
 import { detectObjects } from "./detectObjects.js";
 import { loadRoom1 } from "./room1.js";
+import { loadCorridor } from './corridor.js';
 
 let scene, camera, renderer;
 
 const clock = new THREE.Clock();
-let objects = {};
-let actions = {};
-let mixers = {};
+
 
 let arrowClicked = false;
 
@@ -33,34 +32,34 @@ function onWindowResize(){
     camera.updateProjectionMatrix();
     renderer.setSize( window.innerWidth, window.innerHeight );
 
-    if (Object.keys(objects).length > 0){
+    if (Object.keys(corridorObjects).length > 0){
         let posZ;
         
         // Paladin
         posZ = 0.375 * window.innerWidth - 1150;
         posZ = Math.min(-600, posZ);
         posZ = Math.max(-1000, posZ);
-        objects["paladin"].position.set(objects["paladin"].position.x, objects["paladin"].position.y, posZ);
+        corridorObjects["paladin"].position.set(corridorObjects["paladin"].position.x, objcorridorObjectsects["paladin"].position.y, posZ);
         
         // Door
-        for (let i = 0; i < objects["doors"].length; i++) {
+        for (let i = 0; i < corridorObjects["doors"].length; i++) {
             if (i < 2){
                 posZ = 0.67 * window.innerWidth - 2333;
             }else{
                 posZ = 0.67 * window.innerWidth - 4333;
             }
-            objects["doors"][i].position.set(objects["doors"][i].position.x, objects["doors"][i].position.y, posZ);
+            corridorObjects["doors"][i].position.set(corridorObjects["doors"][i].position.x, corridorObjects["doors"][i].position.y, posZ);
         }
 
 
         // Door texts
-        for (let i = 0; i < objects["doorTexts"].length; i++) {
+        for (let i = 0; i < corridorObjects["doorTexts"].length; i++) {
             if (i < 2){
                 posZ = 0.67 * window.innerWidth - 1933;
             }else{
                 posZ = 0.67 * window.innerWidth - 3933;
             }
-            objects["doorTexts"][i].position.set(objects["doorTexts"][i].position.x, objects["doorTexts"][i].position.y, posZ);
+            corridorObjects["doorTexts"][i].position.set(corridorObjects["doorTexts"][i].position.x, corridorObjects["doorTexts"][i].position.y, posZ);
         }
     
     }          
@@ -85,58 +84,9 @@ controls.addEventListener('change', renderer);
 controls.minDistance = 500;
 controls.maxDistance = 1500;
 
-// Load the corridor
-let corridorMaterialArray = [];
-let texture_ft = new THREE.TextureLoader().load( 'img/wall2.jpg');
-let texture_bk = new THREE.TextureLoader().load( 'img/wall2.jpg');
-let texture_up = new THREE.TextureLoader().load( 'img/wall3.jpg');
-let texture_dn = new THREE.TextureLoader().load( 'img/parquet.jpg');
-let texture_rt = new THREE.TextureLoader().load( 'img/wall2.jpg');
-let texture_lf = new THREE.TextureLoader().load( 'img/wall2.jpg');
 
-texture_dn.wrapS = texture_dn.wrapT = THREE.RepeatWrapping;
-texture_up.wrapS = texture_up.wrapT = THREE.RepeatWrapping;
-texture_ft.wrapS = texture_ft.wrapT = THREE.RepeatWrapping;
-texture_bk.wrapS = texture_bk.wrapT = THREE.RepeatWrapping;
 
-texture_dn.repeat.y = 5;
-texture_up.repeat.y = 5;
-texture_ft.repeat.x = 5;
-texture_bk.repeat.x = 5;
 
-corridorMaterialArray.push(new THREE.MeshBasicMaterial( { map: texture_ft }));
-corridorMaterialArray.push(new THREE.MeshBasicMaterial( { map: texture_bk }));
-corridorMaterialArray.push(new THREE.MeshBasicMaterial( { map: texture_up }));
-corridorMaterialArray.push(new THREE.MeshBasicMaterial( { map: texture_dn }));
-corridorMaterialArray.push(new THREE.MeshBasicMaterial( { map: texture_rt }));
-corridorMaterialArray.push(new THREE.MeshBasicMaterial( { map: texture_lf }));
-
-for (let i = 0; i < 6; i++) {
-    corridorMaterialArray[i].side = THREE.BackSide;
-}
-    
-let corridorGeo = new THREE.BoxGeometry( 1000, 1000, 10000);
-let corridor = new THREE.Mesh( corridorGeo, corridorMaterialArray );
-corridor.position.set(0, 0, -4500)
-scene.add( corridor );  
-
-// Load the top arrow 
-const arrowLoader = new FBXLoader();
-arrowLoader.load('models/arrow.fbx', (arrow) => {
-    arrow.traverse(child => {
-    child.castShadow = true;
-    child.receiveShadow = true;
-    child.material = new THREE.MeshPhongMaterial( { 
-        color: 0xff0000
-    } );
-    });       
-    arrow.scale.setScalar(200);
-    arrow.position.set(0, 200, -600);
-    arrow.rotation.set(-Math.PI/2, Math.PI, Math.PI/2);
-    arrow.name = "arrow";
-    objects["arrow"] = arrow;
-    scene.add(arrow);
-});
 
 // Get the mouse position
 function onMouseMove( event ) {
@@ -152,7 +102,7 @@ function onClick(event) {
     for ( let i = 0; i < intersects.length; i ++ ) {
         if ((intersects[0].object.name == 'Box') || (intersects[0].object.name == 'Box1')){
             
-            if (Object.keys(objects).length > 1){
+            if (Object.keys(corridorObjects).length > 1){
                 arrowClicked = true;
             }
         }
@@ -160,169 +110,22 @@ function onClick(event) {
         if (intersects[0].object.name.substring(0, 4) == 'door'){
             let num = parseInt(intersects[0].object.name.charAt(4), 10); 
             
-            if (objects["doors"][num] != undefined){
+            if (corridorObjects["doors"][num] != undefined){
                 camera.position.set(1400, 100, 0);
                 camera.lookAt( room1Objects["box2"].position );
-                hemiLight.position.set(0, -10000, 0);
-                dirLight.position.set(0, -10000, 300);
+                corridorLights["hemiLight"].position.set(0, -10000, 0);
+                corridorLights["dirLight"].position.set(0, -10000, 300);
             }
 
         }
     }
 }
 
-// Hemisphere Light
-const hemiLight = new THREE.HemisphereLight( 0xffffff, 0x444444 );
-hemiLight.position.set(0, 0, 0);
-scene.add( hemiLight );
-
-// Directional Light
-const dirLight = new THREE.DirectionalLight( 0xffffff );
-dirLight.position.set(0, 0, 300);
-dirLight.castShadow = true;
-dirLight.shadow.camera.top = 180;
-dirLight.shadow.camera.bottom = - 100;
-dirLight.shadow.camera.left = - 120;
-dirLight.shadow.camera.right = 120;
-scene.add( dirLight );
-
-// Load the paladin model
-const loaderPaladin = new FBXLoader();
-loaderPaladin.load('models/paladin/paladin.fbx', (paladin) => {
-
-    // Load the model
-    paladin.scale.setScalar(3);
-    paladin.position.set(0, -500, -600)
-    paladin.rotation.set(0, Math.PI, 0)
-    paladin.traverse(child => {
-    child.castShadow = true;
-    child.receiveShadow = true;
-    });
-    objects["paladin"] = paladin;
-
-    // Load the walk animation
-    const animWalk = new FBXLoader();
-    animWalk.load('models/paladin/walk.fbx', (animWalk) => {
-        const mixer = new THREE.AnimationMixer(paladin);     
-        const action = mixer.clipAction( animWalk.animations[0] );
-        mixers["mixerWalk"] = mixer
-        actions["walk"] = action;
-    });
-    
-    // Load the stand animation
-    const animStand = new FBXLoader();
-    animStand.load('models/paladin/stand_brief.fbx', (animStand) => {
-        const mixer = new THREE.AnimationMixer(paladin);
-        const action = mixer.clipAction(animStand.animations[0]);
-        mixers["mixerStand"] = mixer
-        actions["stand"] = action;
-        action.play();
-    });
-
-    // Load the stand animation
-    const animRightTurn = new FBXLoader();
-    animRightTurn.load('models/paladin/right_turn.fbx', (animRightTurn) => {
-        const mixer = new THREE.AnimationMixer(paladin);
-        const action = mixer.clipAction(animRightTurn.animations[0]);
-        action.loop = THREE.LoopOnce;
-        action.clampWhenFinished = true;
-        mixers["mixerRightTurn"] = mixer
-        mixer.timeScale = 0.4;
-                mixer.addEventListener( 'finished', finishRightTurn );
-        actions["rightTurn"] = action;
-    });
-
-    scene.add(paladin);
-});
-
-let rightTurnFinished = false;
-
-function finishRightTurn(){
-    rightTurnFinished = true;
-    actions["rightTurn"].reset()
-}
-
-
-// Load the door model
-let doors = [];
-var mat;
-let doorPosX, doorPosZ;
-for ( let i = 0; i < 4; i ++ ) {
-    const loaderDoor = new FBXLoader();
-    loaderDoor.load('models/door.fbx', (door) => {
-        
-        door.traverse(child => {
-            child.castShadow = true;
-            child.receiveShadow = true;
-            mat = child.material;
-            child.name = "door" + i.toString();
-        });
-        door.scale.setScalar(0.3);
-
-        if (i % 2 == 0){
-            doorPosX = -500;
-            door.rotation.set(0, Math.PI/2, 0);
-        }else{
-            doorPosX = 500;
-            door.rotation.set(0, -Math.PI/2, 0);
-        }
-    
-        if (i < 2){
-            doorPosZ = 0.67 * window.innerWidth - 2333;
-        }else{
-            doorPosZ = 0.67 * window.innerWidth - 4333;
-        }
-
-        door.position.set(doorPosX, -500, doorPosZ);
-        scene.add(door);
-        doors.push(door);
-    });
-}
-objects["doors"] = doors;
-
-
-// Load text
-let doorTexts = [];
-let doorTextPosX, doorTextPosZ;
-let texts = ['Profil', 'Formation', 'Experiences', 'Projets']
-for ( let i = 0; i < 4; i ++ ) {
-    const loaderText = new THREE.FontLoader();
-    loaderText.load( 'fonts/Bebas_Neue_Regular.json', function ( font ) {
-
-        const textGeometry = new THREE.TextGeometry( texts[i], {
-            font: font, size: 60, height: 2,
-        });
-        
-        var textMaterial = new THREE.MeshPhongMaterial( { 
-            color: 0x00ff00, 
-        });
-        
-        var mesh = new THREE.Mesh( textGeometry, textMaterial );
-
-        if (i % 2 == 0){
-            doorTextPosX = -480;
-        }else{
-            doorTextPosX = 210;
-        }
-
-        if (i < 2){
-            doorTextPosZ = 0.67 * window.innerWidth - 1933;
-        }else{
-            doorTextPosZ = 0.67 * window.innerWidth - 3933;
-        }
-
-        mesh.position.set(doorTextPosX, 150, doorTextPosZ)
-        scene.add( mesh );
-
-        
-        doorTexts.push(mesh);
-    } );
-}
-
-objects["doorTexts"] = doorTexts;
 
 
 
+
+let [corridorObjects, corridorMaterials, corridorMixers, corridorActions, corridorLights] = loadCorridor(scene);
 let room1Objects = loadRoom1(scene);
 
 
@@ -333,33 +136,33 @@ function animate() {
 
     // Hover objects
     if (arrowClicked == false){
-        detectObjects(scene, raycaster, mouse, camera, objects, mat);
+        detectObjects(scene, raycaster, mouse, camera, corridorObjects, corridorMaterials["door"]);
     }
 
     // Walk
     if (arrowClicked == true){
-        walkTo(positionState, objects, actions, camera);
+        walkTo(positionState, corridorObjects, corridorActions, camera);
     }
 
     // Stop walking at the second position
-    if ((arrowClicked == true) && (objects["paladin"].position.z < -2500) && (positionState == 0)){
-        stopWalk(positionState, objects, actions);
+    if ((arrowClicked == true) && (corridorObjects["paladin"].position.z < -2500) && (positionState == 0)){
+        stopWalk(positionState, corridorObjects, corridorActions);
         arrowClicked = false;
         positionState = 1;
     } 
 
     // Stop walking at first position
-    if ((arrowClicked == true) && (positionState == 1) && (objects["paladin"].position.z > -600)){
-        stopWalk(positionState, objects, actions);
+    if ((arrowClicked == true) && (positionState == 1) && (corridorObjects["paladin"].position.z > -600)){
+        stopWalk(positionState, corridorObjects, corridorActions);
         arrowClicked = false;
         positionState = 0;
     } 
 
     // Update mixers
     const delta = clock.getDelta();
-    if ( mixers["mixerWalk"] ) mixers["mixerWalk"].update( delta );
-    if ( mixers["mixerStand"] ) mixers["mixerStand"].update( delta );
-    if ( mixers["mixerRightTurn"] ) mixers["mixerRightTurn"].update( delta );
+    if ( corridorMixers["mixerWalk"] ) corridorMixers["mixerWalk"].update( delta );
+    if ( corridorMixers["mixerStand"] ) corridorMixers["mixerStand"].update( delta );
+    if ( corridorMixers["mixerRightTurn"] ) corridorMixers["mixerRightTurn"].update( delta );
 
     requestAnimationFrame(animate);
 
